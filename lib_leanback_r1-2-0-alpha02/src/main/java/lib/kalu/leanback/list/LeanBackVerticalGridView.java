ceb5a8ba84db4bfb0a5bc753dad81890580ba33c
kalu <153437803@qq.com>
2023-02-15 16:27:01 +0800

    update api

--- lib_leanback_r1-2-0-alpha02/src/main/java/lib/kalu/leanback/list/LeanBackVerticalGridView.java
+++ lib_leanback_r1-2-0-alpha02/src/main/java/lib/kalu/leanback/list/LeanBackVerticalGridView.java
@@ -2,9 +2,13 @@ package lib.kalu.leanback.list;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.FocusFinder;
+import android.view.View;
+import android.view.ViewParent;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class LeanBackVerticalGridView extends androidx.leanback.widget.VerticalGridView {
 
@@ -31,5 +35,57 @@ public class LeanBackVerticalGridView extends androidx.leanback.widget.VerticalG
         setAnimateChildLayout(false);
         setHasFixedSize(true);
         setFocusableInTouchMode(false);
+    }
+
+    public final void scrollUp(int row) {
+        while (true) {
+            View focusedChild = getFocusedChild();
+            if (null == focusedChild)
+                break;
+            int height = focusedChild.getHeight();
+            scrollBy(0, -height);
+            View nextFocus = FocusFinder.getInstance().findNextFocus(this, focusedChild, View.FOCUS_UP);
+            if (null == nextFocus)
+                break;
+            while (true) {
+                ViewParent parent = nextFocus.getParent();
+                if (parent instanceof LeanBackVerticalGridView) {
+                    break;
+                }
+                nextFocus = (View) parent;
+            }
+            if (null == nextFocus)
+                break;
+            nextFocus.requestFocus();
+            int position = getChildAdapterPosition(nextFocus);
+            if (position == row)
+                break;
+        }
+    }
+
+    public final void scrollDown(int row) {
+        while (true) {
+            View focusedChild = getFocusedChild();
+            if (null == focusedChild)
+                break;
+            int height = focusedChild.getHeight();
+            scrollBy(0, height);
+            View nextFocus = FocusFinder.getInstance().findNextFocus(this, focusedChild, View.FOCUS_UP);
+            if (null == nextFocus)
+                break;
+            while (true) {
+                ViewParent parent = nextFocus.getParent();
+                if (parent instanceof LeanBackVerticalGridView) {
+                    break;
+                }
+                nextFocus = (View) parent;
+            }
+            if (null == nextFocus)
+                break;
+            nextFocus.requestFocus();
+            int position = getChildAdapterPosition(nextFocus);
+            if (position == row)
+                break;
+        }
     }
 }
