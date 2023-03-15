818daac9468cc0687dfff2d6aa77b5de61121d95
kalu <153437803@qq.com>
2023-03-15 13:30:25 +0800

    update api

--- lib_leanback_r1-2-0-alpha02/src/main/java/lib/kalu/leanback/popu/PopuTextView.java
+++ lib_leanback_r1-2-0-alpha02/src/main/java/lib/kalu/leanback/popu/PopuTextView.java
@@ -2,16 +2,19 @@ package lib.kalu.leanback.popu;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.leanback.R;
@@ -21,26 +24,52 @@ import androidx.leanback.R;
  */
 @SuppressLint("AppCompatCustomView")
 public final class PopuTextView extends TextView {
+
+    private final Rect mRect = new Rect();
+    private final RectF mRectF = new RectF();
+    private final Path mPath = new Path();
+
+    @ColorInt
+    private int mBackgroundColor = Color.WHITE;
+    private int mMarginBottom = 0;
+    private int mCorners = 0;
+
     public PopuTextView(Context context) {
         super(context);
+        init(context, null);
     }
 
     public PopuTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs);
     }
 
     public PopuTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context, attrs);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public PopuTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
     }
 
-    private final Rect mRect = new Rect();
-    private final RectF mRectF = new RectF();
-    private final Path mPath = new Path();
+    private void init(Context context, @Nullable AttributeSet attrs) {
+        TypedArray typed = null;
+        try {
+            typed = context.obtainStyledAttributes(attrs, R.styleable.PopuTextView);
+            mBackgroundColor = typed.getColor(R.styleable.PopuTextView_ptv_background_color, Color.WHITE);
+            mMarginBottom = typed.getDimensionPixelOffset(R.styleable.PopuTextView_ptv_margin_bottom, 0);
+            mCorners = typed.getDimensionPixelOffset(R.styleable.PopuTextView_ptv_corners, 0);
+        } catch (Exception e) {
+            mBackgroundColor = Color.WHITE;
+            mMarginBottom = 0;
+        }
+        if (null != typed) {
+            typed.recycle();
+        }
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -52,16 +81,16 @@ public final class PopuTextView extends TextView {
         mRectF.left = mRect.left;
         mRectF.right = mRect.right;
         mRectF.top = mRect.top;
-        mRectF.bottom = mRect.bottom * 0.9f;
+        mRectF.bottom = mRect.bottom - mMarginBottom;
         // 3-popu
         Paint paint = getPaint();
-        paint.setColor(Color.WHITE);
+        paint.setColor(mBackgroundColor);
         paint.setAntiAlias(true);
         paint.setStrokeWidth(1);
         paint.setStyle(Paint.Style.FILL_AND_STROKE);
         paint.setStrokeCap(Paint.Cap.ROUND);
         paint.setStrokeJoin(Paint.Join.ROUND);
-        int rx = getResources().getDimensionPixelOffset(R.dimen.module_poputextview_margin);
+        int rx = mCorners;
         canvas.drawRoundRect(mRectF, rx, rx, paint);
         // 4
         int centerX = mRect.centerX();
@@ -78,7 +107,7 @@ public final class PopuTextView extends TextView {
         canvas.drawPath(mPath, paint);
     }
 
-//    @Override
-//    public void setBackground(Drawable background) {
-//    }
+    @Override
+    public void setBackground(Drawable background) {
+    }
 }
