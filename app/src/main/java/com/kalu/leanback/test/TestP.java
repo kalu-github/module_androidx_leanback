31efd289692ecf718f337445bde91b95b4eeed17
kalu <153437803@qq.com>
2023-12-26 14:43:16 +0800

    update files

---
+++ app/src/main/java/com/kalu/leanback/test/TestP.java
@@ -0,0 +1,24 @@
+package com.kalu.leanback.test;
+
+import lib.kalu.leanback.presenter.ListTvGridPresenter;
+
+public class TestP extends ListTvGridPresenter<Object> {
+    @Override
+    protected int initLayout(int i) {
+        return 0;
+    }
+
+    @Override
+    protected int initSpan() {
+        return 0;
+    }
+//    @Override
+//    protected int initLayout(int i) {
+//        return 0;
+//    }
+//
+//    @Override
+//    protected int initSpan() {
+//        return 0;
+//    }
+}
