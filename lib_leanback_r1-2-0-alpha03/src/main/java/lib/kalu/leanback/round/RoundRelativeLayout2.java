72b2fb40dd8b663922c9b531f64755a9cad2824c
kalu <153437803@qq.com>
2024-11-29 18:20:57 +0800

    update files

--- lib_leanback_r1-2-0-alpha03/src/main/java/lib/kalu/leanback/round/RoundRelativeLayout2.java
+++ lib_leanback_r1-2-0-alpha03/src/main/java/lib/kalu/leanback/round/RoundRelativeLayout2.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
@@ -14,12 +15,17 @@ import androidx.annotation.Nullable;
 import androidx.core.view.ViewCompat;
 import androidx.leanback.R;
 
+import lib.kalu.leanback.util.LeanBackUtil;
+
 public class RoundRelativeLayout2 extends RelativeLayout implements RoundImpl {
 
     public float mScale = 1.05f;
     public int mDuration = 100;
+
     private Paint mPaint;
-    private float[] mRadii;
+    private int mStrokeWidth;
+    private int mStrokeColor;
+
     private int mCorner;
     private int mCornerTopLeft;
     private int mCornerTopRight;
@@ -28,29 +34,36 @@ public class RoundRelativeLayout2 extends RelativeLayout implements RoundImpl {
 
     public RoundRelativeLayout2(Context context) {
         super(context);
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
+        init(context, null);
     }
 
     public RoundRelativeLayout2(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context, attrs);
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
+        init(context, null);
     }
 
     public RoundRelativeLayout2(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context, attrs);
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
+        init(context, null);
     }
 
     @SuppressLint("NewApi")
     public RoundRelativeLayout2(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        init(context, attrs);
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
+        init(context, null);
     }
 
     public void init(@NonNull Context context, @NonNull AttributeSet attrs) {
-        setLayerType(LAYER_TYPE_SOFTWARE, null);
         TypedArray typedArray = null;
         try {
             typedArray = context.obtainStyledAttributes(attrs, R.styleable.RoundView2);
+//            mStrokeColor = typedArray.getColor(R.styleable.RoundView2_rv_stroke_width, Color.WHITE);
+            mStrokeColor = Color.RED;
+            mStrokeWidth = typedArray.getDimensionPixelOffset(R.styleable.RoundView2_rv_stroke_width, 0);
             mCorner = typedArray.getDimensionPixelOffset(R.styleable.RoundView2_rv_corner, 0);
             mCornerTopLeft = typedArray.getDimensionPixelOffset(R.styleable.RoundView2_rv_corner_top_left, 0);
             mCornerTopRight = typedArray.getDimensionPixelOffset(R.styleable.RoundView2_rv_corner_top_right, 0);
@@ -59,7 +72,7 @@ public class RoundRelativeLayout2 extends RelativeLayout implements RoundImpl {
             mScale = typedArray.getFloat(R.styleable.RoundView2_rv_scale, 1.05f);
             mDuration = typedArray.getInteger(R.styleable.RoundView2_rv_duration, 100);
         } catch (Exception e) {
-            e.printStackTrace();
+            LeanBackUtil.log("RoundImageView -> init -> Exception -> " + e.getMessage(), e);
         }
 
         if (null != typedArray) {
@@ -74,21 +87,20 @@ public class RoundRelativeLayout2 extends RelativeLayout implements RoundImpl {
         if (null == mPaint) {
             mPaint = new Paint();
         }
-        if (null == mRadii) {
-            // radii dx,dy 成对出现，控制上右下左，四个位置圆角
-            if (mCorner > 0) {
-                mRadii = new float[]{mCorner, mCorner, mCorner, mCorner, mCorner, mCorner, mCorner, mCorner};
-            } else {
-                mRadii = new float[]{mCornerTopLeft, mCornerTopLeft, mCornerTopRight, mCornerTopRight, mCornerBottomRight, mCornerBottomRight, mCornerBottomLeft, mCornerBottomLeft};
-            }
-        }
-      //  clipPath(canvas, mPaint, mRadii, mClipStrokeWidth);
-//        drawFocus(canvas, mPaint, mRadii, mFocusStrokeWidth);
+        int corner1 = mCornerTopLeft > 0 ? mCornerTopLeft : mCorner;
+        clipCornerTopLeft(canvas, mPaint, corner1);
+        int corner2 = mCornerTopRight > 0 ? mCornerTopRight : mCorner;
+        clipCornerTopRight(canvas, mPaint, corner2);
+        int corner3 = mCornerBottomLeft > 0 ? mCornerBottomLeft : mCorner;
+        clipCornerBottomLeft(canvas, mPaint, corner3);
+        int corner4 = mCornerBottomRight > 0 ? mCornerBottomRight : mCorner;
+        clipCornerBottomRight(canvas, mPaint, corner4);
+        drawBorder(canvas, mPaint, mStrokeWidth, mStrokeColor, corner1, corner2, corner3, corner4);
     }
 
-    @Override
-    protected void onFocusChanged(boolean gainFocus, int direction, @Nullable Rect previouslyFocusedRect) {
-        super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
-        ViewCompat.animate(this).scaleX(gainFocus ? mScale : 1f).scaleY(gainFocus ? mScale : 1f).setDuration(mDuration).start();
-    }
+//    @Override
+//    protected void onFocusChanged(boolean gainFocus, int direction, @Nullable Rect previouslyFocusedRect) {
+//        super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
+//        ViewCompat.animate(this).scaleX(gainFocus ? mScale : 1f).scaleY(gainFocus ? mScale : 1f).setDuration(mDuration).start();
+//    }
 }
